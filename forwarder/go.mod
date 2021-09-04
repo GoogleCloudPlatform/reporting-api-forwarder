0c7787b4a0aab3b48bbe6a8bbc00dac89689ab05
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2021-09-04 20:44:42 +0900

    fix(forwarder): parse JSON and embed all attributes to Int64Counter

--- forwarder/go.mod
+++ forwarder/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/rs/zerolog v1.23.0
+	go.opentelemetry.io/otel v1.0.0-RC1
 	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.22.0
 	go.opentelemetry.io/otel/metric v0.22.0
 	go.opentelemetry.io/otel/sdk/metric v0.22.0
--- forwarder/main.go
+++ forwarder/main.go
@@ -19,9 +19,11 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
@@ -47,7 +49,11 @@ func init() {
 		metric.WithInstrumentationVersion(instrumentationVersion),
 	)
 	var err error
-	reportCounter, err = meter.NewInt64Counter("request.count")
+	reportCounter, err = meter.NewInt64Counter(
+		"report.count",
+		metric.WithDescription("number of reports"),
+		metric.WithUnit("call"),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -86,20 +92,20 @@ func main() {
 	defer shutdown()
 
 	e := echo.New()
+	e.HideBanner = true
+	// In order to hand Reporting API, the reporting endpoint needs to handle CORS
 	e.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 	e.GET("/", rootHandler)
 	e.POST("/main", mainHandler)
 	e.POST("/default", defaultHandler)
-	if err := e.Start(":3000"); err != nil {
+	if err := e.StartTLS(":30443", "cert/cert.pem", "cert/key.pem"); err != nil {
 		logger.Fatal().Msgf("failure occured during HTTP server launch process: %v", err)
 	}
 }
 
 func rootHandler(c echo.Context) error {
 	r := c.Request()
-	ctx := r.Context()
 
-	reportCounter.Add(ctx, 1)
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		logger.Error().Msgf("failed to read request body: %v", err)
@@ -119,7 +125,10 @@ func defaultHandler(c echo.Context) error {
 }
 
 func handleReportRequest(c echo.Context) error {
+	now := time.Now()
 	r := c.Request()
+	ctx := r.Context()
+
 	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/reports+json" {
 		logger.Error().Msgf("Content-Type header is not application/reports+json: %v", r.Header)
@@ -132,12 +141,86 @@ func handleReportRequest(c echo.Context) error {
 	}
 	defer r.Body.Close()
 
-	var buf []map[string]interface{}
+	var buf []report
 	err = json.Unmarshal(data, &buf)
 	if err != nil {
 		logger.Error().Msgf("error on parsing JSON: %v", err)
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
-	logger.Info().Msgf("%v", buf)
+	for _, r := range buf {
+		kvs := extractKeyValues(r, now)
+		reportCounter.Add(ctx,
+			1,
+			kvs...,
+		)
+	}
 	return c.String(http.StatusOK, "OK")
+}
+
+// extractKeyValues returns a slice of KeyValues based on the metadata
+// embedded in body in the report.
+func extractKeyValues(r report, t time.Time) []attribute.KeyValue {
+	// NOTE: from Go1.17, use time.Time#UnixMilli
+	now := t.UnixNano() / int64(time.Millisecond)
+	kvs := make([]attribute.KeyValue, 4)
+	kvs[0] = attribute.String("type", r.Typ)
+	kvs[1] = attribute.String("url", r.URL)
+	kvs[2] = attribute.String("useragent", r.UserAgent)
+	kvs[3] = attribute.Int64("generated", now-int64(r.Age))
+	body := r.Body
+	switch r.Typ {
+	case "csp-violation":
+		kvs = append(kvs,
+			attribute.String("blocked-url", body["blockedURL"].(string)),
+			attribute.String("dispotision", body["disposition"].(string)),
+			attribute.String("document-url", body["documentURL"].(string)),
+			attribute.String("effective-directive", body["effectiveDirective"].(string)),
+			attribute.String("original-policy", body["originalPolicy"].(string)),
+			attribute.String("referrer", body["referrer"].(string)),
+			attribute.String("sample", body["sample"].(string)),
+			attribute.Int("status-code", int(body["statusCode"].(float64))),
+		)
+	case "deprecation":
+		kvs = append(kvs,
+			attribute.Int("column-number", int(body["columnNumber"].(float64))),
+			attribute.Int("line-number", int(body["lineNumber"].(float64))),
+			attribute.String("id", body["id"].(string)),
+			attribute.String("message", body["message"].(string)),
+			attribute.String("source-file", body["sourceFile"].(string)),
+		)
+	case "permissions-policy-violation":
+		kvs = append(kvs,
+			attribute.Int("column-number", int(body["columnNumber"].(float64))),
+			attribute.Int("line-number", int(body["lineNumber"].(float64))),
+			attribute.String("dispotision", body["disposition"].(string)),
+			attribute.String("message", body["message"].(string)),
+			attribute.String("policy-id", body["policyId"].(string)),
+			attribute.String("source-file", body["sourceFile"].(string)),
+		)
+	case "document-policy-violation":
+		kvs = append(kvs,
+			attribute.Int("column-number", int(body["columnNumber"].(float64))),
+			attribute.Int("line-number", int(body["lineNumber"].(float64))),
+			attribute.String("dispotision", body["disposition"].(string)),
+			attribute.String("message", body["message"].(string)),
+			attribute.String("policy-id", body["policyId"].(string)),
+			attribute.String("source-file", body["sourceFile"].(string)),
+		)
+	case "coep":
+		kvs = append(kvs,
+			attribute.String("blocked-url", body["blockedURL"].(string)),
+			attribute.String("dispotision", body["disposition"].(string)),
+			attribute.String("destination", body["destination"].(string)),
+		)
+	case "intervention":
+		kvs = append(kvs,
+			attribute.Int("column-number", int(body["columnNumber"].(float64))),
+			attribute.Int("line-number", int(body["lineNumber"].(float64))),
+			attribute.String("id", body["id"].(string)),
+			attribute.String("message", body["message"].(string)),
+			attribute.String("source-file", body["sourceFile"].(string)),
+		)
+	default:
+	}
+	return kvs
 }
